47225068ce351da7dc1bb5fc76eb10673e6e286b
JosephCooperHN <kj.aleman@gmail.com>
2023-03-14 16:03:06 -0600

    Creando app de ejemplo ClientesKB

---
+++ 2023-01/ClientesKB/app/src/main/java/hn/uth/clienteskb/clientes/ClienteActivity.java
@@ -0,0 +1,41 @@
+package hn.uth.clienteskb.clientes;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import hn.uth.clienteskb.databinding.ActivityClienteBinding;
+
+public class ClienteActivity extends AppCompatActivity {
+
+    private ActivityClienteBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityClienteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
